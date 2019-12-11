bdc4bf0c326e62ffc695b9311d8600d3aecefdc3
gregbast1994 <gregbast1994@gmail.com>
2019-12-11 10:13:09 -0500

    init

---
+++ gerrg-woocommerce-bogo.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:       Gerrg Woocommerce BOGO
+ * Plugin URI:        http://gerrg.com/how-to-code-woocommerce-bogo/
+ * Description:       Allows the admin to implement BOGO marketing rules
+ * Version:           0.5
+ * Author:            Gerrg
+ * Author URI:        http://gerrg.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       gerrg
+ * Domain Path:       /languages
+ */
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
